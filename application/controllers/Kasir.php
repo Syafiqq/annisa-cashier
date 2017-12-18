86a8494e08104f4d4c5688cf6a36cc66d7961524
Syafiqq <id.muhammad.syafiq@gmail.com>
2017-12-19 05:50:37 +0700

    Finishing kasir controller

--- application/controllers/Kasir.php
+++ application/controllers/Kasir.php
@@ -1,5 +1,12 @@
 <?php
 
+/**
+ * @property CI_Session session
+ * @property M_transaksi m_transaksi
+ * @property CI_Input input
+ * @property M_transaksi_m m_transaksi_m
+ * @property M_transaksi_d m_transaksi_d
+ */
 class Kasir extends CI_Controller
 {
 
@@ -25,7 +32,35 @@ class Kasir extends CI_Controller
     {
         if ($this->session->userdata('status') == "login" and $this->session->userdata('level') == "kasir")
         {
+            $this->load->model(['m_transaksi_m', 'm_transaksi_d']);
+            $data = [];
+            if (strcmp($this->input->post('status'), 'ya') === 0)
+            {
+                $data['no_meja'] = $this->input->post('table');
+            }
+            if (!is_null($this->input->post('note')))
+            {
+                $data['keterangan'] = $this->input->post('note');
+            }
+            $data['grand_total'] = $this->input->post('cost');
+            $data['id_user']     = $this->session->userdata('id');
+            $data['id_outlet']   = $this->session->userdata('outlet');
 
+            $id = $this->m_transaksi_m->insert($data);
+            foreach ($this->input->post('goods') as $item)
+            {
+                $data                = [];
+                $data['transaction'] = $id;
+                $data['product']     = $item['i'];
+                $data['quantity']    = $item['q'];
+                $data['total']       = intval($item['q']) * intval($item['c']);
+                $this->m_transaksi_d->insert($data);
+            }
+            echo json_encode(['m' => ['Transaksi Berhasil'], 's' => 1]);
+        }
+        else
+        {
+            echo json_encode(['m' => ['Akses Ditolak'], 's' => 0]);
         }
     }
 
