73af776f81f713ff5e81435d0452ab8c7a451f15
Syafiqq <id.muhammad.syafiq@gmail.com>
2017-12-19 05:49:50 +0700

    Add M transaksi d model

---
+++ application/models/M_transaksi_d.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * This <annisa.com> project created by :
+ * Name         : syafiq
+ * Date / Time  : 19 December 2017, 5:26 AM.
+ * Email        : syafiq.rezpector@gmail.com
+ * Github       : syafiqq
+ */
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+/**
+ * @property CI_DB_query_builder|CI_DB_pdo_driver db
+ */
+class M_transaksi_d extends CI_Model
+{
+    public function __construct()
+    {
+        parent::__construct();
+        // Your own constructor code
+    }
+
+    /**
+     * @param $data
+     * @return int
+     */
+    public function insert($data)
+    {
+        $this->db->set('id_tm', $data['transaction']);
+        $this->db->set('id_produk', $data['product']);
+        $this->db->set('tanggal', 'CURRENT_TIMESTAMP', false);
+        $this->db->set('jumlah', $data['quantity']);
+        $this->db->set('total', $data['total']);
+        $this->db->insert('`transaksi_d`');
+
+        return $this->db->insert_id('transaksi_d_id_seq');
+    }
+}
+
+?>
