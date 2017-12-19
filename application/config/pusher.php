a62f6326e22a14edc4ebe7b34f6f18b2c7c8a216
Syafiqq <id.muhammad.syafiq@gmail.com>
2017-12-20 05:46:38 +0700

    Add pusher config

---
+++ application/config/pusher.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * This <annisa.com> project created by :
+ * Name         : syafiq
+ * Date / Time  : 20 December 2017, 5:45 AM.
+ * Email        : syafiq.rezpector@gmail.com
+ * Github       : syafiqq
+ */
+
+$config['pusher_app_id']  = '447344';
+$config['pusher_key']     = '7dd372a2fbe222be06f6';
+$config['pusher_secret']  = 'b60893c7f201af087174';
+$config['pusher_cluster'] = 'ap1';
+?>
