b7d8f87b35c3e6fe240d2ea5aa73b5311b50a5c3
Syafiqq <id.muhammad.syafiq@gmail.com>
2017-12-23 15:17:33 +0700

    Change m_produk parent to my_model

--- application/models/M_produk.php
+++ application/models/M_produk.php
@@ -1,8 +1,13 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class M_produk extends CI_Model
+class M_produk extends MY_Model
 {
+    public function __construct()
+    {
+        parent::__construct('`produk`');
+    }
+
 
     function getProduk()
     {
