dbfa08362d898a81d0a784dcb33f111ebb5933a4
Syafiqq <id.muhammad.syafiq@gmail.com>
2017-12-23 17:53:55 +0700

    REgister item update

--- application/config/routes.php
+++ application/config/routes.php
@@ -49,6 +49,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
-$route['default_controller']   = 'Login';
-$route['404_override']         = '';
-$route['translate_uri_dashes'] = false;
+$route['default_controller']    = 'Login';
+$route['api/dapur/item/update'] = 'api/dapur/item_update';
+$route['404_override']          = '';
+$route['translate_uri_dashes']  = false;
