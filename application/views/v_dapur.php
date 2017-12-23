95bcf50e17ac461625cacef1d9c4bff5c5e44b57
Syafiqq <id.muhammad.syafiq@gmail.com>
2017-12-23 16:57:37 +0700

    Add notify dependency Add baseurl meta Add Dummy loader

--- application/views/v_dapur.php
+++ application/views/v_dapur.php
@@ -19,11 +19,13 @@ Website: http://thevectorlab.net/
     <meta content="width=device-width, initial-scale=1.0" name="viewport"/>
     <meta content="" name="description"/>
     <meta content="" name="author"/>
+    <meta name="base-url" content="<?php echo base_url() ?>">
     <link href="<?php echo base_url(); ?>assets/assets/bootstrap/css/bootstrap.min.css" rel="stylesheet"/>
     <link href="<?php echo base_url(); ?>assets/assets/font-awesome/css/font-awesome.css" rel="stylesheet"/>
     <link href="<?php echo base_url(); ?>assets/css/style.css" rel="stylesheet"/>
     <link href="<?php echo base_url(); ?>assets/css/style_responsive.css" rel="stylesheet"/>
     <link href="<?php echo base_url(); ?>assets/css/style_default.css" rel="stylesheet" id="style_color"/>
+    <link href="<?php echo site_url(); ?>assets/css/bootstrap-dialog.min.css" rel="stylesheet"/>
     <style>
         div.queue-container {
             margin: 40px
@@ -55,6 +57,7 @@ Website: http://thevectorlab.net/
 <div class="login-header">
     <?php $this->load->view('headerDapur.php') ?>
 </div>
+<button id="dummy-load" class="button">Load</button>
 <div class="container" style="width: 100%">
     <div class="row">
         <div class="span12">
@@ -110,7 +113,7 @@ Website: http://thevectorlab.net/
         </div>
         <div class="span4">
             <div class="request-container">
-                <table class="table">
+                <table id="request-list" class="table">
                     <thead>
                     <tr>
                         <th>Pesanan</th>
@@ -141,15 +144,54 @@ Website: http://thevectorlab.net/
 <script src="<?php echo base_url(); ?>assets/js/jquery.blockui.js"></script>
 <script src="<?php echo base_url(); ?>assets/js/pusher.min.js"></script>
 <script src="<?php echo base_url(); ?>assets/js/scripts.js"></script>
+<script type="text/javascript" src="<?php echo base_url(); ?>assets/js/bootstrap-notify.min.js"></script>
+<script type="text/javascript" src="<?php echo base_url(); ?>assets/js/bootstrap-dialog.min.js"></script>
 <script>
     jQuery(document).ready(function () {
-        App.initLogin();
+        //App.initLogin();
     });
 </script>
 <!-- END JAVASCRIPTS -->
 <script>
     (function ($) {
         $(function () {
+
+            var s_request = 'table#request-list';
+            var products  = <?php echo json_encode(isset($products) ? $products : [])?>;
+            var queues    = {};
+            var request   = {};
+
+            $('button#dummy-load').on('click', function () {
+                $.post(
+                    $('meta[name=base-url]').attr('content') + 'api/dapur/load',
+                    null,
+                    null,
+                    'json')
+                    .done(function (response) {
+                        if (response !== undefined)
+                        {
+                            if (response['n'] !== undefined)
+                            {
+                                for (var i = -1, is = response['n'].length; ++i < is;)
+                                {
+                                    $.notify({
+                                        message: response['n'][i]
+                                    }, {
+                                        type: 'info'
+                                    });
+                                }
+                            }
+                            if ((response['r'] !== undefined) && (response['r']['pesanan'] !== undefined))
+                            {
+                                queues = response['r']['pesanan'];
+                            }
+                        }
+                    })
+                    .fail(function (error) {
+                    })
+                    .always(function (error) {
+                    });
+            })
 // Enable pusher logging - don't include this in production
             /*Pusher.logToConsole = true;
 
