4b75f8614687b0931abfff6aad50d20c65519cc6
Syafiqq <id.muhammad.syafiq@gmail.com>
2017-12-23 17:53:43 +0700

    Change to associative array

--- application/controllers/api/Dapur.php
+++ application/controllers/api/Dapur.php
@@ -13,6 +13,7 @@
  * @property CI_Loader load
  * @property M_transaksi_m m_transaksi_m
  * @property M_transaksi_d m_transaksi_d
+ * @property CI_Input input
  */
 class Dapur extends CI_Controller
 {
@@ -21,31 +22,39 @@ class Dapur extends CI_Controller
         parent::__construct();
         if ($this->session->userdata('status') != "login")
         {
-            echo json_encode(['n' => ['Request Forbidden']]);
+            echo json_encode(['n' => ['Request Forbidden'], 's' => 0]);
 
             return;
         }
     }
 
-    function load()
+    public function load()
     {
         /** @noinspection PhpParamsInspection */
         $this->load->model(['m_transaksi_m', 'm_transaksi_d']);
         $this->m_transaksi_m->find(function (CI_DB_query_builder $db) {
             $db->select();
             $db->where('DATE(`tanggal`) = DATE(NOW())', null, false);
+            $db->order_by('`tanggal`', 'ASC');
         });
-        $queues = $this->m_transaksi_m->getResult()->result_array();
-        foreach ($queues as &$queue)
+        $queues = [];
+        foreach ($this->m_transaksi_m->getResult()->result_array() as $queue)
         {
             $this->m_transaksi_d->find(function (CI_DB_query_builder $db) use ($queue) {
                 $db->select();
                 $db->where('`id_tm`', $queue['id_tm']);
             });
-            $queue['pesanan'] = $this->m_transaksi_d->getResult()->result_array();
+            $queue['pesanan'] = [];
+            foreach ($this->m_transaksi_d->getResult()->result_array() as $request)
+            {
+                $queue['pesanan']["r_{$request['id_produk']}"] = $request;
+            }
+            $queues["q_{$queue['id_tm']}"] = $queue;
         }
-        echo json_encode(['n' => ['Antrian Ditambahkan'], 'r' => ['pesanan' => $queues]]);
+        echo json_encode(['n' => ['Antrian Ditambahkan'], 'r' => ['pesanan' => $queues], 's' => 1]);
     }
+
+
 }
 
 ?>
