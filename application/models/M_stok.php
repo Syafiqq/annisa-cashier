266ffd2333442ea067ecd80830bf053bea2e05e1
Syafiqq <id.muhammad.syafiq@gmail.com>
2017-12-24 18:28:47 +0700

    Add M_stok

---
+++ application/models/M_stok.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * This <annisa.com> project created by :
+ * Name         : syafiq
+ * Date / Time  : 24 December 2017, 6:28 PM.
+ * Email        : syafiq.rezpector@gmail.com
+ * Github       : syafiqq
+ */
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class M_stok extends MY_Model
+{
+    public function __construct()
+    {
+        parent::__construct('`stok`');
+        // Your own constructor code
+    }
+}
+
+?>
